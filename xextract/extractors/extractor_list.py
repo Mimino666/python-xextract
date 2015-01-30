b8fc797786da07012fedd26d4fd49b5439cee895
Michal Danilak <mimino.coder@gmail.com>
2015-01-30 09:45:50 +0100

    Deleted `flatten()` method.

--- xextract/extractors/extractor_list.py
+++ xextract/extractors/extractor_list.py
@@ -1,12 +1,9 @@
-from xextract.utils.python import flatten
-
-
 class XPathExtractorList(list):
     def __getslice__(self, i, j):
         return self.__class__(list.__getslice__(self, i, j))
 
     def select(self, xpath):
-        return self.__class__(flatten(x.select(xpath) for x in self))
+        return self.__class__(node for extractor in self for node in extractor.select(xpath))
 
     def extract(self):
         return [x.extract() for x in self]
