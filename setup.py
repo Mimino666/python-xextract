c3efe49334f35cf7f8a7522a122d69dec904ade1
Michal Danilak <mimino.coder@gmail.com>
2016-04-14 15:58:54 +0200

    Dumped to v0.1.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='xextract',
-    version='0.1.0',
+    version='0.1.1',
     description='Extract structured data from HTML and XML documents like a boss.',
     long_description=readme,
     author='Michal "Mimino" Danilak',
